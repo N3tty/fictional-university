0d5e12f7fea5bf7ec8bd5caf7591aefd3c4bc48c
Natalie <nlezon@gmail.com>
2021-03-24 15:09:30 +0000

    Professor Post Types - Images

--- wp-content/themes/fictional-university-theme/single-program.php
+++ wp-content/themes/fictional-university-theme/single-program.php
@@ -21,8 +21,47 @@ while (have_posts()){
 <div class="generic-content">
 <?php the_content(); ?>
 </div>
-
 <?php
+//List of Professors teaching program
+$relatedProfessors = new WP_Query(array(
+  'posts_per_page'=>-1,
+  'post_type'=>'professor',
+  'orderby'=>'title',
+  'order'=>'ASC',
+  'meta_query'=>array(
+
+    array(
+      'key' => 'related_programs',
+      'compare' => 'LIKE',
+      'value' => '"'.get_the_ID().'"'
+
+    )
+  )
+));
+
+if ($relatedProfessors->have_posts())
+{
+  echo '<hr class="section-break">';
+  echo '<h2 class="headline headline--medium">'. get_the_title(). ' Professors:</h2>';
+echo '<ul class="professor-cards">';
+  while($relatedProfessors->have_posts())
+  {
+    $relatedProfessors->the_post(); ?>
+
+
+<li class="professor-card__list-item">
+  <a class="professor-card" href="<?php the_permalink();?>">
+    <img class="professor-card__image" src="<?php the_post_thumbnail_url('professorLandscape'); ?>">
+    <span class="professor-card__name"><?php the_title();?></span>
+  </a></li>
+
+
+    <?php
+  }
+echo '</ul>';
+}
+wp_reset_postdata();
+//List Program Events
 $today=date('Ymd');
 $homepageEvents = new WP_Query(array(
   'posts_per_page'=>2,
@@ -48,7 +87,8 @@ $homepageEvents = new WP_Query(array(
 
 if ($homepageEvents->have_posts())
 {
-  echo '<hr class="sction-break">';
+
+  echo '<hr class="section-break">';
   echo '<h2 class="headline headline--medium">Upcoming '. get_the_title(). ' Events:</h2>';
   while($homepageEvents->have_posts())
   {
@@ -60,6 +100,7 @@ if ($homepageEvents->have_posts())
         echo $eventDate->format('M'); ?></span>
         <span class="event-summary__day"><?php echo $eventDate->format('d'); ?></span>
       </a>
+
       <div class="event-summary__content">
         <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink();?>"><?php the_title();?></a></h5>
         <p><?php if (has_excerpt())
